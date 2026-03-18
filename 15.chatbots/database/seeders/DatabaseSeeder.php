ccd27e939816812f043a1e5c7928344d3bbcfafe
Luis Arrieta <luisarrieta796@gmail.com>
2026-03-17 19:34:28 -0500

    Update UserFactory password and seed database with specific user details

--- 15.chatbots/database/seeders/DatabaseSeeder.php
+++ 15.chatbots/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->withPersonalTeam()->create();
 
         User::factory()->withPersonalTeam()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Luis Arrieta',
+            'email' => 'luisarrieta796@gmail.com',
         ]);
     }
 }
