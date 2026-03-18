5bb77f9f0564f9763961aeea9d1e5ea85f0c6e07
Luis Arrieta <luisarrieta796@gmail.com>
2026-03-18 08:57:59 -0500

    Add ChatbotsForm component for editing chatbot details and update Edit page to use it

--- 15.chatbots/resources/js/Pages/Chatbots/Edit.vue
+++ 15.chatbots/resources/js/Pages/Chatbots/Edit.vue
@@ -1,22 +1,29 @@
 <script setup>
-import { Link, usePoll } from '@inertiajs/vue3'
+import { Link, useForm } from '@inertiajs/vue3'
 import { ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
-import { ref } from 'vue'
 import AppLayout from '@/Layouts/AppLayout.vue'
 import FormSection from '@/Components/FormSection.vue'
 import PrimaryButton from '@/Components/PrimaryButton.vue'
 import ActionMessage from '@/Components/ActionMessage.vue'
+import ChatbotsForm from '@/Components/Chatbots/ChatbotsForm.vue'
 
-const showModal = ref(false)
-
-usePoll(5000)
-
-defineProps({
+const props = defineProps({
   chatbot: {
     type: Object,
     required: true,
   },
 })
+
+const form = useForm({
+  name: props.chatbot.name || '',
+  system_prompt: props.chatbot.system_prompt || '',
+  model: props.chatbot.model || '',
+  temperature: String(props.chatbot.temperature) || '0.7',
+})
+
+const handleSubmit = () => {
+  form.put(route('chatbots.update', props.chatbot.id))
+}
 </script>
 
 <template>
@@ -39,10 +46,12 @@ defineProps({
           <ArrowUturnLeftIcon class="size-4" />
           <span>Volver al listado</span>
         </Link>
-        <FormSection>
+        <FormSection @submitted="handleSubmit">
           <template #title>Editar Chatbot</template>
           <template #description> Edita la información del chatbol. </template>
-          <template #form>FORM FIELDS</template>
+          <template #form>
+            <ChatbotsForm :form="form" />
+          </template>
           <template #actions>
             <ActionMessage :on="false" class="me-3">
               Actualizado.
