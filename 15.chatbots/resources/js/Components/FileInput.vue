4fe760f16ce37ca341bc1da2222722b1742befc8
Luis Arrieta <luisarrieta796@gmail.com>
2026-03-18 11:33:45 -0500

    Refactor FileInput component to improve file handling and UI integration

--- 15.chatbots/resources/js/Components/FileInput.vue
+++ 15.chatbots/resources/js/Components/FileInput.vue
@@ -1,11 +1,12 @@
 <script setup>
+import { XCircleIcon } from '@heroicons/vue/24/solid'
 import { onMounted, ref } from 'vue'
 
 defineProps({
   modelValue: Object,
 })
 
-defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue'])
 
 const input = ref(null)
 
@@ -16,13 +17,29 @@ onMounted(() => {
 })
 
 defineExpose({ focus: () => input.value.focus() })
+
+const clearFile = () => {
+  input.value.value = null
+  emit('update:modelValue', null)
+}
 </script>
 
 <template>
-  <input
-    ref="input"
-    type="file"
-    class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
-    @input="$emit('update:modelValue', $event.target.files[0])"
-  />
+  <div class="relative flex items-center justify-between">
+    <input
+      ref="input"
+      type="file"
+      v-bind="$attrs"
+      class="filer:rounded rounded-md border border-gray-300 pr-8 shadow-sm file:mr-2 file:cursor-pointer file:border-none file:bg-gray-200 file:px-3 file:py-2 file:text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 file:dark:bg-gray-700 file:dark:text-white dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+      @input="$emit('update:modelValue', $event.target.files[0])"
+    />
+    <button
+      v-if="modelValue"
+      class="text-white-500 dark:text-white-700 absolute right-2 top-3"
+      type="button"
+      @click="clearFile"
+    >
+      <XCircleIcon class="size-5" />
+    </button>
+  </div>
 </template>
