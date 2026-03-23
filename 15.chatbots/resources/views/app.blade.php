dbb7930f343332064471d07a2345c3f8d9f80065
Luis Arrieta <luisarrieta796@gmail.com>
2026-03-23 15:53:45 -0500

    Add streaming dependencies and CSRF token

--- 15.chatbots/resources/views/app.blade.php
+++ 15.chatbots/resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
