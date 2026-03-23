6cdabdac2a9ad9ec7875bc239db6cc31a298d417
Luis Arrieta <luisarrieta796@gmail.com>
2026-03-23 10:36:08 -0500

    Add Enter key support to TextArea for form submission

--- 15.chatbots/resources/js/Components/TextArea.vue
+++ 15.chatbots/resources/js/Components/TextArea.vue
@@ -18,6 +18,14 @@ onMounted(() => {
 })
 
 defineExpose({ focus: () => textarea.value.focus() })
+
+const handleEnterPressed = event => {
+  if (event.shiftKey) return
+
+  event.preventDefault()
+
+  event.target.form.dispatchEvent(new Event('submit'))
+}
 </script>
 
 <template>
@@ -25,6 +33,7 @@ defineExpose({ focus: () => textarea.value.focus() })
     ref="textarea"
     class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
     :value="modelValue"
+    @keydown.enter="handleEnterPressed"
     @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
